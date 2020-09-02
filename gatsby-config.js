fc56adb62e763b000f31165f16560f1dff4c1ea3
Steven Yang <skewerblack@gmail.com>
2020-09-01 21:47:43 -0700

    Update gatsby-config.js
    Change Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Future Fest`,
+    description: `Supercharging your live streamed shows!`,
+    author: `Future Fest Inc.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
