0506afa41b7da0ee05043674b27b4afdcf2f4da1
Steven Yang <skewerblack@gmail.com>
2020-09-01 22:12:13 -0700

    Update index.js
    Add video player in a different method

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,19 @@ Amplify.configure(config)
 const IndexPage = () => (
   <Layout>
     <h1>Welcome to the Future of Digital Events</h1>
-    <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
-    <video width="320" height="240" controls> <source src="https://4f8770ea05fa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.390409532441.channel.S02Iw01flH87.m3u8" type="application/x-mpegURL"></video>
+    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+    <video id="video"></video>
+    <script>
+    if(Hls.isSupported()) {
+      var video = document.getElementById('video');
+      var hls = new Hls();
+      hls.loadSource('https://4f8770ea05fa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.390409532441.channel.S02Iw01flH87.m3u8');
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+      });
+    }
+    </script>
   </Layout>
 )
 
